e841db9b70fe94e75241d941dd03ca28e9b87b8a
unknown <2801611461@qq.com>
2025-10-01 00:34:28 +0800

    feat:P42T01的代码实现

---
+++ 02Chapter/Linked_list/P4201.cpp
@@ -0,0 +1,31 @@
+#include "node.h"
+#include <iostream>
+#include <vector>
+/*
+   《2026王道数据结构》综合题P42T01;
+    function:Delete the element with a value of X in the singly linked list with the leading node and free up space；
+    Time Complexity :O(n);
+    Space Complexity:O(1);
+*/
+template <typename T>
+using Node = SinglyNode<T>;
+
+template <typename T>
+void solve(Node<T>* head, T x)
+{
+    if (head == nullptr) {
+        std::cerr << "错误：表头节点指针为空！" << std::endl;
+        return;
+    }
+
+    Node<T>* t = head;
+    while (t->next != nullptr) {
+        if (t->next->data == x) {
+            Node<T>* del = t->next;
+            t->next = del->next;
+            delete del;
+            continue;
+        }
+        t = t->next;
+    }
+}
